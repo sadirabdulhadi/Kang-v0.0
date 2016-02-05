7ba66efd7f2b86ff89d721a85ca7ebebbd71a486
Sadir <sadeer11@gmail.com>
2016-02-05 22:53:39 +0000

    added page question1

---
+++ background-image+/Question1.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  background-image+
+//
+//  Created by Sadir Abdulhadi on 2/4/16.
+//  Copyright (c) 2016 UCL. All rights reserved.
+//
+
+import UIKit
+
+class Question1: UIViewController {
+    @IBAction func Scoring(sender: AnyObject) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        /* signUpAsTherapistButton.layer.cornerRadius = 10*/
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
