67befe81bde51b4e613321eb346c5c921049434d
Sadir <sadeer11@gmail.com>
2016-02-06 23:34:45 +0000

    fixedConstraintsQuestion1

--- background-image+/Question1.swift
+++ background-image+/Question1.swift
@@ -8,19 +8,14 @@
 
 import UIKit
 
-class question1: UIViewController {
-    
-    override func viewDidLoad() {
+class Question1: UIViewController {
+        override func viewDidLoad() {
         super.viewDidLoad()
-        /* signUpAsTherapistButton.layer.cornerRadius = 10*/
-        
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+           }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+            }
     
     
 }
---
+++ background-image+/Question2.swift
@@ -0,0 +1,9 @@
+//
+//  Question2.swift
+//  background-image+
+//
+//  Created by Sadir Abdulhadi on 2/6/16.
+//  Copyright (c) 2016 UCL. All rights reserved.
+//
+
+import Foundation
