bd5b0906df582b074ce92a73958dd0a112efbc57
Sadir <sadeer11@gmail.com>
2016-02-11 14:43:40 +0000

    added-aboutYou-Therapage

---
+++ background-image+/TellUsThera.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  background-image+
+//
+//  Created by Sadir Abdulhadi on 2/4/16.
+//  Copyright (c) 2016 UCL. All rights reserved.
+//
+
+import UIKit
+
+class TellUsThera: UIViewController {
+   
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    
+}
+
