425a7f54d5570140560f6d7ffc0b12a97ceaef9a
Sadir <sadeer11@gmail.com>
2016-02-14 23:22:21 +0000

    added-realm

--- background-image+/TellUsThera.swift
+++ background-image+/TellUsThera.swift
@@ -9,21 +9,26 @@
 import UIKit
 
 class TellUsThera: UIViewController {
-   
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        self.view.endEditing(true)
-    }
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    
+    func textFieldShouldReturn(scrollView: UIScrollView!) -> Bool {
+        self.view.endEditing(true)
+        scrollView.resignFirstResponder()
+        return true;
+    }
     
 }
 
